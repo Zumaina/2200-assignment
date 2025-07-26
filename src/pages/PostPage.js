2da644a1c822c432150ac9e8d6531dfeb95e04be
Zumaina <zumainatahsincat@gmail.com>
2025-07-27 00:42:55 +0600

    add 2 comments

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,21 +1,18 @@
 import { Link } from "react-router-dom";
 import ReactionBar from "../components/ReactionBar";
+import CommentBox from "../components/CommentBox";
 
 const PostPage = () => {
-  const dummyAuthorId = "123"; // Example ID
+  const dummyAuthorId = "123";
 
   return (
     <div>
       <h1>This is Post Page</h1>
-
       <p>
-        Author:{" "}
-        <Link to={`/author/${dummyAuthorId}`}>
-          John Doe
-        </Link>
+        Author: <Link to={`/author/${dummyAuthorId}`}>John Doe</Link>
       </p>
-
       <ReactionBar />
+      <CommentBox />
     </div>
   );
 };
